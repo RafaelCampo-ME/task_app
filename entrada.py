4291355b82c4c33184dbdb69df3b727413daee25
Rafael <rs.campo10@uniandes.edu.co>
2022-08-30 13:07:36 -0500

    cambio dataclass

--- entrada.py
+++ entrada.py
@@ -1,23 +1,27 @@
-import random 
+import random
+from sqlite3 import Timestamp 
 import string
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 
 ##Voy a construir una clase de registro que guarde un registro contable
 
 
 def generate_id() -> str:
+    print("Id generado")
     return ''.join(random.choices(string.ascii_uppercase, k=12))
 
 @dataclass(frozen=False)
-class Person:
-    name: str
-    adress: str
-    active: bool = True  ##Valor de default
+class AccountingEntry:
+    entry_id: str = field(default_factory=generate_id())
+    date_created: str
+    merchant: str
+    entry_descrip: str
+    entry_amount: str
+    entry_class: str = "Gastos"
+    meta_data: list[str] = field(default_factory=list)  ##Funcion
 
 
+###Meterle suave de validacion para los campos entry_amount y merchant 
 
-    def __str__(self) -> str:
-        return f"{self.name},{self.address}"
-
-generate_id()
-
+ 
+ 
